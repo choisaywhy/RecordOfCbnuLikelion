1f28c4318880790a4bd6345968885c76856b465f
EunjiShin <angel9803@naver.com>
2019-02-22 23:13:04 +0900

    eunji

--- likelion/account/forms.py
+++ likelion/account/forms.py
@@ -2,5 +2,17 @@ from django import forms
 from django.contrib.auth import models as auth_models
 
 class LoginForm(forms.Form):
-    username = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control',}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control',}))
+    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'ID'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder':'Password'}))
+    fields = ('username', 'password',)  
+    
+    def __init__(self, *args, **kwargs):
+        super(LoginForm, self).__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].help_text = None
+            self.fields[field].label = ''
+            self.fields[field].error = None
+        
+
+
+
